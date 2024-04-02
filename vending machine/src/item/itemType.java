33b3a2dcaada492ff5f786dd27a109f870208d88
KHUSHBU KUMARI <131510344+Khushbu8130@users.noreply.github.com>
2024-04-02 21:59:37 +0530

    Update itemType.java

--- vending machine/src/item/itemType.java
+++ vending machine/src/item/itemType.java
@@ -1,14 +1,14 @@
 package item;
 
 public enum ItemType {
-    OREO("OREO"), // i = 0,8
-    JUICE("JUICE"), // i =1,9
-    COKE("COKE"), // i =2 
-    SODA("SODA"), // i = 3
-    CHIPS("CHIPS"), // i = 4
-    CHOCOLATE("CHOCOLATE"), // i =5
-    CANDY("CANDY"), // i =6
-    GUM("GUM"); // i=7
+    OREO("OREO"), 
+    JUICE("JUICE"), 
+    COKE("COKE"), 
+    SODA("SODA"), 
+    CHIPS("CHIPS"), 
+    CHOCOLATE("CHOCOLATE"), 
+    CANDY("CANDY"), 
+    GUM("GUM"); 
 
     private final String type;
     ItemType (String type) {
